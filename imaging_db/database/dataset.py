c23542500715928b7aec5515027cb5db7974e23e
Jenny <jenny.folkesson@gmail.com>
2018-07-13 11:51:52 -0700

    test data storage improvement

--- imaging_db/database/dataset.py
+++ imaging_db/database/dataset.py
@@ -11,11 +11,13 @@ class DataSet(Base):
     id = Column(Integer, primary_key=True)
     dataset_serial = Column(String)
     description = Column(String)
+    microscope = Column(String)
     sliced = Column(Boolean)
     parent_id = Column(Integer, ForeignKey("data_set.id"))
 
-    def __init__(self, dataset_serial, description, sliced, parent_id):
+    def __init__(self, dataset_serial, description, microscope, sliced, parent_id):
         self.dataset_serial = dataset_serial
         self.description = description
+        self.microscope = microscope
         self.sliced = sliced
         self.parent_id = parent_id
