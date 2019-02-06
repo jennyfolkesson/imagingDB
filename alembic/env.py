d0912bec7f7574d88b588915f5172f34e1566d15
Jenny <jenny.folkesson@gmail.com>
2019-02-06 09:17:27 -0800

    baseline database revision

---
+++ alembic/env.py
@@ -0,0 +1,85 @@
+from __future__ import with_statement
+from alembic import context
+import os
+from sqlalchemy import engine_from_config, pool
+from logging.config import fileConfig
+
+import imaging_db.database.db_session as db_session
+import imaging_db.metadata.json_validator as json_validator
+
+
+# Edit this depending on where your database credential file is stored
+# This assumes it's stored in dir above imagingDB
+dir_name = os.path.abspath(os.path.join('..'))
+DB_CREDENTIALS_PATH = os.path.join(dir_name, 'db_credentials.json')
+
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Overwrite the ini-file sqlalchemy.url path
+credentials_json = json_validator.read_json_file(
+    json_filename=DB_CREDENTIALS_PATH,
+    schema_name="CREDENTIALS_SCHEMA")
+
+config.set_main_option(
+    'sqlalchemy.url',
+    db_session.json_to_uri(credentials_json=credentials_json))
+
+print("Using url:", config.get_main_option('sqlalchemy.url'))
+
+# Add model metadata object
+target_metadata = db_session.Base.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url, target_metadata=target_metadata, literal_binds=True)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool)
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
---
+++ alembic/versions/818bfb535590_baseline_revision.py
@@ -0,0 +1,24 @@
+"""baseline revision
+
+Revision ID: 818bfb535590
+Revises: 
+Create Date: 2019-02-06 09:03:09.432402
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '818bfb535590'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
