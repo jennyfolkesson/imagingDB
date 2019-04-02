36c90656c2188718e5915391bd53b13a56d9837f
Jenny <jenny.folkesson@gmail.com>
2019-04-01 22:05:53 -0700

    added filename test

--- tests/database/db_operations_tests.py
+++ tests/database/db_operations_tests.py
@@ -32,6 +32,19 @@ class TestDBOperations(db_basetest.DBBaseTest):
         self.s3_dir = 'testing/TEST-2005-10-09-20-00-00-0001'
         self.sha256 = 'aaabbbccc'
 
+        self.frames_meta = meta_utils.make_dataframe(6)
+        self.frames_json_meta = []
+        self.meta_dict = {'local_key': 'local_value'}
+        for i, (c, z) in enumerate(itertools.product(range(3), range(2))):
+            im_name = 'im_c00{}_z00{}_t005_p050.png'.format(c, z)
+            self.frames_meta.loc[i, 'file_name'] = im_name
+            self.frames_meta.loc[i, 'channel_idx'] = c
+            self.frames_meta.loc[i, 'slice_idx'] = z
+            self.frames_meta.loc[i, 'pos_idx'] = 50
+            self.frames_meta.loc[i, 'time_idx'] = 5
+            self.frames_meta.loc[i, 'sha256'] = self.sha256
+            self.frames_json_meta.append(self.meta_dict)
+
     def tearDown(self):
         super().tearDown()
 
@@ -48,42 +61,45 @@ class TestDBOperations(db_basetest.DBBaseTest):
         db_inst = db_ops.DatabaseOperations(
             dataset_serial=self.dataset_serial,
         )
-        frames_meta = meta_utils.make_dataframe(6)
-        frames_json_meta = []
-        meta_dict = {'local_key': 'local_value'}
-        for i, (c, z) in enumerate(itertools.product(range(3), range(2))):
-            im_name = 'im_c{}_z{}_t005_p050.png'.format(c, z)
-            frames_meta.loc[i, 'file_name'] = im_name
-            frames_meta.loc[i, 'channel_idx'] = c
-            frames_meta.loc[i, 'slice_idx'] = z
-            frames_meta.loc[i, 'pos_idx'] = 50
-            frames_meta.loc[i, 'time_idx'] = 5
-            frames_meta.loc[i, 'sha256'] = self.sha256
-            frames_json_meta.append(meta_dict)
         db_inst.insert_frames(
             session=self.session,
             description='test frames',
-            frames_meta=frames_meta,
-            frames_json_meta=frames_json_meta,
+            frames_meta=self.frames_meta,
+            frames_json_meta=self.frames_json_meta,
             global_meta=self.global_meta,
             global_json_meta=self.global_json_meta,
             microscope=self.microscope,
             parent_dataset=None,
         )
+        # query frames
         all_frames = self.session.query(db_ops.Frames) \
             .join(db_ops.FramesGlobal) \
             .join(db_ops.DataSet) \
             .filter(db_ops.DataSet.dataset_serial == self.dataset_serial) \
             .order_by(db_ops.Frames.file_name)
         for i, (c, z) in enumerate(itertools.product(range(3), range(2))):
-            im_name = 'im_c{}_z{}_t005_p050.png'.format(c, z)
+            im_name = 'im_c00{}_z00{}_t005_p050.png'.format(c, z)
             self.assertEqual(all_frames[i].file_name, im_name)
             self.assertEqual(all_frames[i].channel_idx, c)
             self.assertEqual(all_frames[i].slice_idx, z)
             self.assertEqual(all_frames[i].time_idx, 5)
             self.assertEqual(all_frames[i].pos_idx, 50)
             self.assertEqual(all_frames[i].sha256, self.sha256)
-            self.assertDictEqual(all_frames[i].metadata_json, meta_dict)
+            self.assertDictEqual(all_frames[i].metadata_json, self.meta_dict)
+        # query frames_global
+        global_query = self.session.query(db_ops.FramesGlobal) \
+            .join(db_ops.DataSet) \
+            .filter(db_ops.DataSet.dataset_serial == self.dataset_serial)
+        self.assertEqual(global_query[0].s3_dir, self.global_meta['s3_dir'])
+        self.assertEqual(global_query[0].nbr_frames, self.global_meta['nbr_frames'])
+        self.assertEqual(global_query[0].im_width, self.global_meta['im_width'])
+        self.assertEqual(global_query[0].im_height, self.global_meta['im_height'])
+        self.assertEqual(global_query[0].nbr_slices, self.global_meta['nbr_slices'])
+        self.assertEqual(global_query[0].nbr_channels, self.global_meta['nbr_channels'])
+        self.assertEqual(global_query[0].nbr_positions, self.global_meta['nbr_positions'])
+        self.assertEqual(global_query[0].nbr_timepoints, self.global_meta['nbr_timepoints'])
+        self.assertEqual(global_query[0].im_colors, self.global_meta['im_colors'])
+        self.assertEqual(global_query[0].bit_depth, self.global_meta['bit_depth'])
 
     def test_insert_file(self):
         dataset_serial = 'TEST-2005-10-12-20-00-00-0001'
@@ -111,3 +127,25 @@ class TestDBOperations(db_basetest.DBBaseTest):
         self.assertEqual(date_time.day, 12)
         self.assertEqual(dataset.microscope, self.microscope)
         self.assertEqual(dataset.description, self.description)
+
+    def test_get_filenames(self):
+        db_inst = db_ops.DatabaseOperations(
+            dataset_serial=self.dataset_serial,
+        )
+        db_inst.insert_frames(
+            session=self.session,
+            description='test frames',
+            frames_meta=self.frames_meta,
+            frames_json_meta=self.frames_json_meta,
+            global_meta=self.global_meta,
+            global_json_meta=self.global_json_meta,
+            microscope=self.microscope,
+            parent_dataset=None,
+        )
+        s3_dir, file_names = db_inst.get_filenames(
+            session=self.session,
+        )
+        self.assertEqual(s3_dir, self.global_meta['s3_dir'])
+        for i, (c, z) in enumerate(itertools.product(range(3), range(2))):
+            im_name = 'im_c00{}_z00{}_t005_p050.png'.format(c, z)
+            self.assertEqual(file_names[i], im_name)
