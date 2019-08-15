c6db117c40b980b5cc979b0f284a19edecfd6e2e
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2019-08-15 07:14:18 -0700

    add query_data.py to the cli

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
 import os
-from setuptools import setup, find_packages
+
+from setuptools import find_packages, setup
 
 install_requires = [
-    line.rstrip() for line in open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+    line.rstrip() for line in open(
+        os.path.join(os.path.dirname(__file__), "requirements.txt")
+    )
 ]
 
-scripts = ['imaging_db/cli/data_downloader.py', 'imaging_db/cli/data_uploader.py']
+scripts = [
+            'imaging_db/cli/data_downloader.py',
+            'imaging_db/cli/data_uploader.py',
+            'imaging_db/cli/query_data.py'
+]
 
 setup(name='imagingDB',
       install_requires=install_requires,
